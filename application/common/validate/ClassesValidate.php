038171ab4e95309c77c9258d118853edfb3fe101
guowushi <guowushi@qq.com>
2016-08-04 09:56:29 +0800

    增加了admin\common\validate下的验证器类

---
+++ application/common/validate/ClassesValidate.php
@@ -0,0 +1,48 @@
+<?php
+namespace app\common\validate;
+/**
+ * Created by PhpStorm.
+ * User: guowushi
+ * Date: 2016/7/27
+ * Time: 22:07
+ */
+use think\Validate;
+class ClassesValidate  extends Validate
+{
+
+    /**
+     * 定义规则数组
+     * 一条规则包括 验证字段,验证规则函数,错误提示[,验证条件][,附加规则][,验证时间]
+     * 规则的写法有两种
+     * (1)  'age'   => 'number|between:1,120',
+     * (2)  'age'   => ['number','between'=>'1,120'],
+     * @var array
+     */
+    protected $rule = [
+        'class_name'  =>  'require|max:25',
+        'class_room' =>  'require',
+        'class_supervisor' => 'require',
+        'calss_adviser' => 'require'
+    ];
+
+    protected $message  =   [
+        'class_name.require' => '名称必须',
+        'class_name.max'     => '名称最多不能超过25个字符',
+        'class_room'   => '不能为空',
+        'class_supervisor'  => '年龄只能在1-120之间',
+        'calss_adviser'        => '邮箱格式错误',
+    ];
+
+
+    /**
+     * 自定义验证规则
+     * @param $value 实际字段值
+     * @param $rule  规则值
+     * @param $data  数据
+     * @return bool|string
+     */
+    protected function checkName($value,$rule,$data)
+    {
+        // return $rule == $value ? true : '名称错误';
+    }
+}
---
+++ application/common/validate/CourseValidate.php
@@ -0,0 +1,55 @@
+<?php
+namespace app\common\validate;
+/**
+ * Created by PhpStorm.
+ * User: guowushi
+ * Date: 2016/7/27
+ * Time: 22:07
+ */
+use think\Validate;
+
+/**
+ * Course模型的验证类
+ * Class CourseValidate
+ * @package app\common\validate
+ */
+class CourseValidate  extends Validate
+{
+
+    /**
+     * 定义规则数组
+     * 一条规则包括 验证字段,验证规则函数,错误提示[,验证条件][,附加规则][,验证时间]
+     * 规则的写法有两种
+     * (1)  'age'   => 'number|between:1,120',
+     * (2)  'age'   => ['number','between'=>'1,120'],
+     * @var array
+     */
+    protected $rule = [
+        'dept_name'  =>  'require|max:25',
+        'dept_category' =>  'require'
+
+    ];
+    /**
+     * 定义验证规则的提示消息
+     * @var array
+     */
+    protected $message  =   [
+        'dept_name.require' => '名称必须',
+        'dept_name.max'     => '名称最多不能超过25个字符',
+        'dept_category'   => '年龄必须是数字'
+
+    ];
+
+
+    /**
+     * 自定义验证规则(函数名就是规则名)
+     * @param $value 实际字段值
+     * @param $rule  规则值
+     * @param $data  数据
+     * @return bool|string
+     */
+    protected function checkName($value,$rule,$data)
+    {
+        // return $rule == $value ? true : '名称错误';
+    }
+}
