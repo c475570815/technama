5979558837fb97dc55db6ec2e8121b183c8a1911
guowushi <guowushi@qq.com>
2016-08-05 18:02:33 +0800

    增加了THINK_MAIL配置

--- application/config.php
+++ application/config.php
@@ -241,4 +241,15 @@ return [
         // 验证成功后是否重置
         'reset'    => true
     ],
+    //邮件配置
+    'THINK_EMAIL' => array(
+        'SMTP_HOST'   => 'smtp.qq.com', //SMTP服务器
+        'SMTP_PORT'   => '465', //SMTP服务器端口，使用SSL，端口号465或587
+        'SMTP_USER'   => 'guowushi@qq.com', //SMTP服务器用户名
+        'SMTP_PASS'   => 'opiphlbevgjwbebd', //SMTP服务器密码
+        'FROM_EMAIL'  => 'guowushi@qq.com', //发件人EMAIL
+        'FROM_NAME'   => '郭武士', //发件人名称
+        'REPLY_EMAIL' => '', //回复EMAIL（留空则为发件人EMAIL）
+        'REPLY_NAME'  => '', //回复名称（留空则为发件人名称）
+    ),
 ];
