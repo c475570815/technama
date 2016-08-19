ceb3c444784aaeb6a342d4c84b6a409b95d74e70
guowushi <guowushi@qq.com>
2016-08-19 11:26:07 +0800

    删除了一个函数，该函数在common.js中

--- public/scripts/admin/dict.datagrid.js
+++ public/scripts/admin/dict.datagrid.js
@@ -1,25 +1,6 @@
 /**
  * Created by guowushi on 2016/7/22.
  */
-(function($){
-    $.fn.serializeJson=function(){
-        var serializeObj={};
-        var array=this.serializeArray();
-        var str=this.serialize();
-        $(array).each(function(){
-            if(serializeObj[this.name]){
-                if($.isArray(serializeObj[this.name])){
-                    serializeObj[this.name].push(this.value);
-                }else{
-                    serializeObj[this.name]=[serializeObj[this.name],this.value];
-                }
-            }else{
-                serializeObj[this.name]=this.value;
-            }
-        });
-        return serializeObj;
-    };
-})(jQuery);
 
 $(document).ready(function () {
     //当整个页面全部载入后才执行
