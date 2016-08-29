2fe70ae2d60919df6d64ffd65306d41f191ecd98
guowushi <guowushi@qq.com>
2016-08-29 22:20:14 +0800

    增加部门人数统计柱状图

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -2,12 +2,14 @@
 namespace app\admin\controller;
 
 use app\common\model\ConfigModel;
+use app\common\model\DeptModel;
+use app\common\model\TeacherModel;
 use think\Config;
 use think\Controller;
 use app\common\model\TeaModel;
 use think\View;
 use think\Session;
-
+use app\common\helper\VerifyHelper;
 /**
  * admin模块默认的控制器
  * Class Index
@@ -15,18 +17,16 @@ use think\Session;
  */
 class Index extends Controller
 {
-
+    //在执行Action之前需要执行的方法
     protected $beforeActionList = [
-        // 'before'=>  ['except'=>'login,isLogined,authentication']
+//        'before'=>  ['except'=>'login,isLogined,authentication']
     ];
-
     public function before()
     {
         if (!$this->isLogined()) {
             return $this->error("没有登录哦！", 'login');
         }
     }
-
     public function isLogined()
     {
         if (Session::has('uid')) {
@@ -43,7 +43,6 @@ class Index extends Controller
         $view->name = Session::get('name');
         $view->roles = Session::get('roles');
         $view->current_term =$this->getCurrentTerm();
-        // 模板输出
 
         return $view->fetch('layout');
 
@@ -58,14 +57,44 @@ class Index extends Controller
         $row= $model->where('cfg_name','current_term')->find();
         return $row['cfg_term'];
     }
+
+    /**
+     * 部门人员柱状图
+     * @return View
+     */
     public function chart()
     {
-        return view('chart');
-    }
+        $view = new View();
+        $dept=new DeptModel();
+        $rows=$dept->distinct(true)->where("dept_parent","")->field("dept_name")->select();
 
+        foreach ($rows as $row){
+            $xAxis_data[]=$row['dept_name'];
+            $series_data[]=TeacherModel::where('dept_name',$row['dept_name'])->count();
+        }
+        //return json($xAxis_data);
+        $view->xAxis =json_encode($xAxis_data) ;
+        $view->series =implode(",", $series_data);
+
+        return $view->fetch('chart');
+
+    }
+    /**
+     * 显示验证码图片
+     */
+    public function verify()
+    {
+        VerifyHelper::verify();
+    }
+    /**
+     * 登录界面
+     * @return string
+     */
     public function login()
     {
-        return view('login');
+        $view = new View();
+        // 模板输出
+        return  $view->fetch('login');
     }
 
     public function logout()
@@ -91,12 +120,12 @@ class Index extends Controller
         $password = isset($_POST['password']) ? $_POST['password'] : "";
         $captcha = isset($_POST['captcha']) ? $_POST['captcha'] : "";
 
-        //$wher=$this->getfindwhere($statu,$id,$password);
-        $view = new View();
-        if (!captcha_check($captcha)) {
-            //验证失败
-            return $this->error("验证码错误", 'login');
-        };
+//        if (!captcha_check($captcha)) {
+//            //验证失败
+//            $ret=['success'=>'false','message'=>'验证码错误'];
+//            return $ret;
+//            return $this->error("验证码错误", 'login');
+//        };
         if(ldapValid($id,$password)){
             $table = new TeaModel();
             $row = $table->where('teach_id', $id)->find();
@@ -110,9 +139,13 @@ class Index extends Controller
                 // return view('layout');
                 return $this->success("登录成功", 'index');
             } else {
+                $ret=['success'=>'false','message'=>'教师详细信息获取失败'];
+                return $ret;
                 return $this->error("教师详细信息获取失败！", 'login');
             }
         }else{
+            $ret=['success'=>'false','message'=>'登录失败'];
+            return $ret;
             return $this->error("登录失败", 'login');
         }
 
@@ -130,12 +163,44 @@ class Index extends Controller
         $redis->connect('127.0.0.1',6379);
         //$redis->set('test','hello world!');
 
-        for($i=1;$i<1000;$i++){
+        for($i=1;$i<100;$i++){
 //            $redis->lPush('usr', "message $i from server");
-            $redis->publish('first', "message $i from server");
-
+//            $redis->publish('first', "message $i from server");
+//            $redis->publish('SMS_CHANNEL', "SMS message $i from server");
         }
 
+        for($i=1;$i<10 ;$i++){
+            echo $i;
+           $redis->publish('MESSAGE_CHANNEL', " MESSAGE $i from server");
+        }
+
+
+        // 发送email
+
+//        $schedule =  [
+//            'year'=>'2016',  //听课教师
+//            'month' => '8', // 收件列表，多个联系人逗号分开
+//            'day' => '16',      // 标题
+//            'hour' => '4' ,        // html 内容
+//            'min'=>'16',
+//            'sec'=>'0',
+//        ];
+//        $message=[
+//            "subject"=>"测试定时主题",
+//            "to"=>"18942891954@qq.com",
+//            "body"=>"测试邮件内容",
+//            "schedule"=>$schedule
+//        ];
+//
+//
+//        $redis->publish('EMAIL_CHANNEL', json_encode($message,JSON_UNESCAPED_UNICODE));
+//        $message=[
+//            "subject"=>"测试立即主题",
+//            "to"=>"18942891954@qq.com",
+//            "body"=>"测试邮件内容"
+//
+//        ];
+//        $redis->publish('EMAIL_CHANNEL', json_encode($message,JSON_UNESCAPED_UNICODE));
         echo "ok";
     }
 
