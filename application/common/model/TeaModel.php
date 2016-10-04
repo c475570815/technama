49a659f27df536e1cb0720ef40cf05ed13b256ab
guowushi <guowushi@qq.com>
2016-10-04 11:06:18 +0800

    定义了sub_dept查询方式

--- application/common/model/TeaModel.php
+++ application/common/model/TeaModel.php
@@ -22,6 +22,7 @@ class TeaModel extends \app\common\model\CommonModel
         'teach_name' => 'like',
         'teach_pass' => 'like',
         'teach_id' => 'like',
+        'sub_dept' =>'in',
         'dept_name' =>'in'
     ];
     public function isExist($pk){
@@ -33,5 +34,55 @@ class TeaModel extends \app\common\model\CommonModel
             return false;
         }
    //////////////////11111111111
+    }
+    /**
+     * 根据传递过来的对象值，产生查询的条件数组
+     * where('field','opt','condition');
+     * where('name','like','%zhang%');
+     * @param $dict
+     * @return array
+     */
+    public function filer($dict)
+    {
+        $arr_where=array();
+        //dump($dict);
+        foreach($dict as $field => $condition) {
+            //  echo $field.$condition;
+            if ( $condition  <> ''  ) {
+                if($condition <> '全部'){
+                    if(array_key_exists($field,$this->defaults)){
+                        $opt= $this->defaults[$field];
+                        switch ($opt) {
+                            case 'like':
+                                $arr_where[$field] = array(
+                                    $this->defaults[$field],
+                                    '%'.$condition.'%'
+                                );
+                                break;
+                            case 'in':
+                                $arr_where[$field] = array(
+                                    'in',
+                                    $condition
+                                );
+                                break;
+
+                            default:
+                                $arr_where[$field] = array(
+                                    '=',
+                                    $condition
+                                );
+                        }
+                    }else{
+                        $arr_where[$field] = array(
+                            '=',
+                            $condition
+                        );
+                    }
+                }
+            }
+
+        }
+        return  $arr_where;
+
     }
 }
